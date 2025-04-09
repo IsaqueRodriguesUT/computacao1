82590a0ae9de5d84bc21ca86196c1a1804f7345d
IsaqueRodriguesUT <isaquerodrigues@alunos.utfpr.edu.br>
2025-04-09 10:35:03 -0300

    Add files via upload

---
+++ while1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(){
+	int n, x;
+	int i = 1;
+
+	printf("Escreva um numero: ");
+	scanf("%d",&n);
+
+	while(i < 100){
+		x = i % n;
+		if(x == 2){
+			printf("%d\n",i);
+		}
+		i++;
+	}
+	return 0;
+}
