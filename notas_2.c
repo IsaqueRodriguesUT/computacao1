3e14c52d94aaf43d92a5a876e2d690e601deafea
IsaqueRodriguesUT <isaquerodrigues@alunos.utfpr.edu.br>
2025-04-09 07:57:33 -0300

    Add files via upload

--- notas_2.c
+++ notas_2.c
@@ -7,17 +7,17 @@ int main()
 	scanf("%f %f %f", &n1, &n2, &n3);
 
 	printf("Digite a frequência(0 a 100): "); 
-	scanf("%f", freq);
+	scanf("%f", &freq);
 
 	media = (n1 + n2 +n2) / 3;
 
-	if (media > 8){
+	if (media > 8 && freq >= 75.0){
 		printf("Aprovado com Distinção");
 	}
-	else if(media >=6){
+	else if(media >=6 && freq >= 75){
 		printf("Aprovado direto");
 	}
-	else if(media >= 4){
+	else if(media >= 4 && media < 6 && freq >= 75 || media >= 60 && freq < 75 || media >= 4 && freq > 50 && freq < 75){
 		printf("Vai para final");
 	}
 	else{
