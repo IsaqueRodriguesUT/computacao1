639665e95a70f93093b114fe8a0cc7aa8ad2b1c4
IsaqueRodriguesUT <isaquerodrigues@alunos.utfpr.edu.br>
2025-04-16 08:13:12 -0300

    Update while2.c

--- while2.c
+++ while2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main(){
 	int n;
-	float a,b,c;
+	float a,b,c,media;
 
 	printf("digite o numros de casos que você quer fazer a media: \n");
 	scanf("%d",&n);
@@ -9,7 +9,8 @@ int main(){
 	for(int i = 0;i < n;i++){
 		printf("escreva 3 números para fazer a media: \n");
 		scanf("%f %f %f",&a,&b,&c);
-		printf("A média é: %.2f\n", (a + b + c)/3);
+		media = (a*2)+(b*3)+(c*5) / (2+3+5);
+		printf("A média é: %.2f\n", media);
 
 	}
 
