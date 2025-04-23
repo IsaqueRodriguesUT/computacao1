157b5d8c303901b8fec9363ec19c2f0a552373c3
IsaqueRodriguesUT <isaquerodrigues@alunos.utfpr.edu.br>
2025-04-23 10:51:28 -0300

    Add files via upload

--- tabuada.c
+++ tabuada.c
@@ -2,10 +2,18 @@
 int main(){
 	int n;
 
-	printf("digite um número:");
-	scanf("%d",&n);
+	while(1){
+		printf("\nDigite um número: ");
+		if(scanf("%d",&n) != 1){
+			printf("\n---Digite um número----");
+			while(getchar() != '\n');
+		}else{
+			break;
+		}
+		
+	}
 	
-	printf("---- Tabua Do %d ----",n);
+	printf("\n---- Tabua Do %d ----",n);
 	
 	for(int i = 1;i <= 10;i++){
 		printf("\n%d x %d= %d",n,i,n*i);
